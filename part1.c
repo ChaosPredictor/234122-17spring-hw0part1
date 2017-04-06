7697f44f9a1f5915861fe3775c2b55650bfff3b8
chaospredictor <kuzi81@gmail.com>
2017-04-07 00:43:02 +0300

    return The number 4 is a power of... & WRONG Total exponent sum..(due to negative number)

--- part1.c
+++ part1.c
@@ -9,19 +9,39 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef enum { false, true } bool;
+
 int char_to_int(char c);
 int array_to_int(char *c);
 int till_first_enter();
 
 int main() {
 	int number;
-	printf("Enter size of input:");
+	printf("Enter size of input:\n");
 	number = till_first_enter();
 	if ( number == -1) {
 		printf("Invalid size");
 		return 0;
 	}
-	printf("number: %d", number);
+	//printf("number: %d", number);
+	int current_int, sum = 0;
+	printf("Enter numbers:\n");
+	for (int i = 0; i < number; ++i) {
+		current_int = till_first_enter();
+		//printf("number: %d\n", current_int);
+		int i = 2, j = 1;
+		while ( i <= current_int) {
+			if (i == current_int) {
+				printf("The number %d is a power of 2: %d = 2^%d\n", i, i, j);
+			} else {
+				//printf("this is not\n");
+			}
+			i *= 2;
+			j++;
+		}
+		sum += current_int;
+	}
+	printf("Total exponent sum is %d", sum);
 	return 0;
 }
 
@@ -33,9 +53,10 @@ int char_to_int(char c) {
 	return -1;
 }
 
-int till_first_enter(){
-	int c=0, result = 0;
-	while (c != 10){
+inline int till_first_enter(){
+	int c=0, result = 0, i = 0;
+	bool negative = false;
+	while (c != 10 && c != 32){
 		//printf("this is number: %d", result);
 		c = getchar();
 		if ( c >= 48 && c <= 57) {
@@ -45,10 +66,18 @@ int till_first_enter(){
 			//printf("this is number: %d", result);
 			//putchar( c );
 			//printf("\n");
-		} else if ( c != 10) {
-			//printf("this is not a number\n %d", c);
-			return -1;
+		} else if ( c != 10 && c != 32 && c != 45) {
+			//printf("int: %d\n", c);
+			printf("Invalid number\n");
+			return 0;
+		} else if ( c == 45 && i == 0) {
+			negative = true;
+			//printf("negative");
 		}
+		i++;
+	}
+	if (negative) {
+		return - result/10;
 	}
 	return result/10;
 }
