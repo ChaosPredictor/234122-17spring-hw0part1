17e95c7cb13fd636e338c585ec3168f98d8f9421
chaospredictor <kuzi81@gmail.com>
2017-04-07 16:47:10 +0300

    code conventions step2

--- part1.c
+++ part1.c
@@ -9,20 +9,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef enum { false, true } bool;
+typedef enum { false, true } Boolean;
 
 int charToInt(char c);
 int arrayToInt(char *c);
 int tillFirstEnter();
+int removeDoubleSpaces(char *string, int len);
 
 int main() {
 	int number;
+	char str[5];
 	printf("Enter size of input:\n");
-	number = tillFirstEnter();
-	if ( number == -1) {
-		printf("Invalid size");
+	scanf("%s", str);
+	//number = tillFirstEnter();
+	number = arrayToInt(str);
+	if ( number < 1) {
+		printf("Invalid size\n");
 		return 0;
 	}
+
+	char ch;
+    while((ch= getchar()) != '\n' && ch != EOF);
 
 	char *ptr = malloc(sizeof(char) * number * 10);
 	if (ptr == NULL) {
@@ -30,23 +37,26 @@ int main() {
 		exit(1);
 	}
 
-	int c=0, length = 0;
-	char ch;
 	printf("Enter numbers:\n");
-	while (c != 10){
-		c = getchar();
-		ch = (char)c;
-		if ( (c >= 48 && c <= 57) || ((c == 45) || (c == 32))) {
-			ptr[length] = ch;
-		} else if ( c != 10) {
-			//printf("char: %c, int: %d", ch, c);
-			printf("Invalid number\n");
-			return 0;
-		}
-		length++;
+	scanf ("%[^\n]s", ptr);
+
+	//printf("You entered: %s\n", ptr);
+	//printf("sting len: %d\n", strlen(ptr));
+
+	if (checkStringValidity(ptr, strlen(ptr)) == -1) {
+		printf("Invalid number\n");
+		free(ptr);
+		return 0;
+	} else {
+		//printf("Valid String\n");
 	}
+
+
+	int c=0, length = 0;
+
+
 	//printf("STRING1: %s\n" , ptr);
-	int i, j = 1;
+	/*int i, j = 1;
 	for (i = 1; i < length; ++i) {
 		if((int)ptr[j] == 32 && (int)ptr[j-1] == 32) {
 			ptr[i] = ptr[j+1];
@@ -55,12 +65,18 @@ int main() {
 			ptr[i] = ptr[j];
 		}
 		j++;
+	}*/
+
+	if (removeDoubleSpaces(ptr,strlen(ptr)) != -1){
+		//printf("STRING2: %s\n" , ptr);
+		free(ptr);
+		return 0;
 	}
-	//printf("STRING2: %s\n" , ptr);
+
 
 	char* pch = strtok (ptr," ");
 	int current_int, sum = 0, k;
-	while (pch != NULL && k < number)	{
+	while (pch != NULL && k < 100)	{
 		k++;
 		//printf ("%s\n",pch);
 		current_int = arrayToInt(pch);
@@ -76,25 +92,6 @@ int main() {
 		}
 		pch = strtok (NULL, " ");
 	}
-
-
-	//printf("number: %d", number);
-	/*int current_int, sum = 0;
-	printf("Enter numbers:\n");
-	for (int i = 0; i < number; ++i) {
-		current_int = tillFirstEnter();
-		//printf("number: %d\n", current_int);
-		int i = 2, j = 1;
-		while ( i <= current_int) {
-			if (i == current_int) {
-				printf("The number %d is a power of 2: %d = 2^%d\n", i, i, j);
-				sum += j;
-			}
-			i *= 2;
-			j++;
-		}
-
-	}*/
 	printf("Total exponent sum is %d", sum);
 	return 0;
 }
@@ -107,9 +104,40 @@ int charToInt(char c) {
 	return -1;
 }
 
+int removeDoubleSpaces(char *string, int len) {
+	int i, j = 1;
+	for (i = 1; i < len; ++i) {
+		if((int)string[j] == 32 && (int)string[j-1] == 32) {
+			string[i] = string[j+1];
+			i--;
+		} else {
+			string[i] = string[j];
+		}
+		j++;
+	}
+	return i;
+}
+
+int checkStringValidity(const char *string, int len){
+	int i = 0;
+	char tmp;
+	tmp = string[0];
+	while (tmp != 10 && i < len){
+		if ( (tmp >= 48 && tmp <= 57) || ((tmp == 45) || (tmp == 32))) {
+			//printf("%c\n", tmp);
+		} else if ( tmp != 10) {
+			return -1;
+			//printf("Invalid number\n");
+			//return 0;
+		}
+		tmp = string[++i];
+	}
+	return 0;
+}
+
 inline int tillFirstEnter(){
 	int c=0, result = 0, i = 0;
-	bool negative = false;
+	Boolean negative = false;
 	while (c != 10){
 		//printf("this is number: %d", result);
 		c = getchar();
@@ -138,18 +166,22 @@ inline int tillFirstEnter(){
 }
 
 int arrayToInt(char *c){
-	int i = 0,j = 0;
+	int i = 0,tmp = 0;
 	int sum = 0;
-	bool negative = false;
+	Boolean negative = false;
 	if (c[0] == 45) {
 		negative = true;
 		i++;
 	}
 	while (c[i] != 0) {
 		sum *= 10;
-		j = charToInt(c[i]);
-		if (j != -1) sum += j;
-		else return 0;
+		tmp = charToInt(c[i]);
+		if (tmp != -1) {
+			 sum += tmp;
+		}
+		else {
+			return 0;
+		}
 		//printf("%i\n", sum);
 		i++;
 	}
@@ -157,7 +189,4 @@ int arrayToInt(char *c){
 		return -sum;
 	}
 	return sum;
-	//printf("second: %c\n",c[1]);
-	//int i = strlen(c);
-	//return 0;
 }
